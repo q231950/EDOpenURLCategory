ba99ed3dffc667b2df79a4e4c6f51c0c1489963a
Martin Kim Dung-Pham <kim@elbedev.com>
2014-10-24 20:44:19 +0200

    Updated tests

---
+++ Example/Tests/EDOpenURLCategoryTests.m
@@ -0,0 +1,39 @@
+//
+//  EDOpenURLCategoryTests.m
+//  EDOpenURLCategory
+//
+//  Created by Martin Kim Dung-Pham on 24/10/14.
+//  Copyright (c) 2014 Martin Kim Dung-Pham. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EDOpenURLCategoryTests : XCTestCase
+
+@end
+
+@implementation EDOpenURLCategoryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
