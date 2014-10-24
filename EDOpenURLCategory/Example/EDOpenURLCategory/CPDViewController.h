ea450dc0e55b4fef23dbdffb931f461990286c2e
Martin Kim Dung-Pham <kim@elbedev.com>
2014-10-24 19:54:51 +0200

    Added cocoa pod files

---
+++ EDOpenURLCategory/Example/EDOpenURLCategory/CPDViewController.h
@@ -0,0 +1,13 @@
+//
+//  EDViewController.h
+//  EDOpenURLCategory
+//
+//  Created by Martin Kim Dung-Pham on 10/24/2014.
+//  Copyright (c) 2014 Martin Kim Dung-Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDViewController : UIViewController
+
+@end
