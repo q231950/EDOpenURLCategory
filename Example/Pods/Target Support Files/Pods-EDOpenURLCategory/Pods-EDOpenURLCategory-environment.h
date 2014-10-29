aa3d58aefc11bc57684a6c2a2adb42cbac7ba401
Martin Kim Dung-Pham <kim@elbedev.com>
2014-10-29 09:55:32 +0100

    Added build script to .travis.yml and shared Tests scheme

---
+++ Example/Pods/Target Support Files/Pods-EDOpenURLCategory/Pods-EDOpenURLCategory-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// EDOpenURLCategory
+#define COCOAPODS_POD_AVAILABLE_EDOpenURLCategory
+#define COCOAPODS_VERSION_MAJOR_EDOpenURLCategory 0
+#define COCOAPODS_VERSION_MINOR_EDOpenURLCategory 1
+#define COCOAPODS_VERSION_PATCH_EDOpenURLCategory 0
+
